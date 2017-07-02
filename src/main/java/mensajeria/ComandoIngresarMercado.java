310a5bcc551d68d95ebad3d95bffda7c804ac052
Carlos Tomalino <c.ctomalino@gmail.com>
2017-07-01 23:29:46 -0300

    * Se agrega funcionalidad para que cuando un Cliente ingresa al mercado, los demás se enteren y lo quiten de la pantalla si en modo juego o lo agreguen a la lista si están comerciando

---
+++ src/main/java/mensajeria/ComandoIngresarMercado.java
@@ -0,0 +1,28 @@
+package mensajeria;
+
+import java.io.IOException;
+import java.io.Serializable;
+import servidor.EscuchaCliente;
+import servidor.Servidor;
+
+public class ComandoIngresarMercado extends Comando implements Serializable {
+
+	private int idPersonaje;
+
+	@Override
+	public void resolver(EscuchaCliente cliente) {
+
+		// Guardo el estado Comerciando en el personaje
+		Servidor.getPersonajesConectados().get(this.idPersonaje).setComerciando(true);
+
+		// Le aviso a todos los demás clientes que este usuario ingresó al
+		// mercado
+		for (EscuchaCliente clienteConectado : Servidor.getClientesConectados()) {
+			try {
+				clienteConectado.enviarComando(this);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
